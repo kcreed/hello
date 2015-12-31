20870dffad6df43e95344331fe494000e6c8554e
kcreed <kcreed@cox.net>
2015-12-31 06:03:42 -0500

    regex

--- WebContent/Validator.js
+++ WebContent/Validator.js
@@ -15,7 +15,7 @@ function validateForm()
       document.registrationForm.firstname.focus();
       return false;
     } 
-    else if(document.registrationForm.firstname.value != /^[a-zA-Z]+$/)
+    else if(document.registrationForm.firstname.value == /^[0-9]+/)
     {
       alert("First Name cannot contain numbers");
       document.registrationForm.firstname.focus();
